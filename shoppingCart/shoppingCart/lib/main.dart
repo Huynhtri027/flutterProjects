67bac4088c8de6e855a7f17e5853aef5437a455d
akshaymankodia@gmail.com <akshay.mankodia@cognizant.com>
2020-09-01 19:33:28 +0530

    till cart adding done.

--- shoppingCart/shoppingCart/lib/main.dart
+++ shoppingCart/shoppingCart/lib/main.dart
@@ -26,9 +26,6 @@ class MyApp extends StatelessWidget {
         title: 'Flutter Demo',
         initialRoute: '/',
         theme: ThemeData(
-          //https://flutter.dev/docs/development/data-and-backend/state-mgmt/simple
-          //https://medium.com/flutter-community/simple-ways-to-pass-to-and-share-data-with-widgets-pages-f8988534bd5b
-
           primarySwatch: Colors.blue,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
